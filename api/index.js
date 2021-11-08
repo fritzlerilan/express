3ff174b00be7ba5d8bc0f388fb03ca82819b2231
Ilan Fritzler <fritzlerilan@gmail.com>
2021-11-08 04:42:54 -0300

    Port Fix

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import { MPRouter } from "./routes/mercadopago.js";
 
 // Inicialización
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 app.use(express.json());
 
 // Routes
